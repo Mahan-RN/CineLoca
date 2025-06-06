e9e2081edc0006310b2f3362b080f933281805d5
Mahan <mahanrn@student.ubc.ca>
2025-06-05 21:03:17 -0700

    Added Movie class with method specifications, tests, and implementation

---
+++ CineLoca/src/main/Movie.java
@@ -0,0 +1,174 @@
+package main;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Movie {
+    private String imdbID;
+    private String title;
+    private int releaseYear;
+    private String director;
+    private int lengthMinutes;
+    private String countary;
+    private List<String> actors;
+    private boolean englishSubtitle;
+    private String imagePath;
+    private String filePath;
+
+    // EFFECTS: creates a movie object with the given ID, title, path to poster
+    // image and path to the movie file
+    public Movie(String imdbID, String title) {
+        setID(imdbID);
+        setTitle(title);
+        this.actors = new ArrayList<>();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the IMDB ID of this movie
+    // Converts the input to all upper case and then uses replaceAll to remove
+    // all whitespaces
+    public void setID(String imdbID) {
+        String trimmed = imdbID.replaceAll("\\s", "").toUpperCase();
+        this.imdbID = trimmed;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets title of this movie
+    // Strips leading and trailing whitespaces
+    public void setTitle(String title) {
+        this.title = trimString(title);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the release year of this movie
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the given director as the director of this movie
+    // Strips leading and trailing whitespaces
+    public void setDirector(String director) {
+        this.director = trimString(director);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the length of this movie (unit: minutes)
+    public void setLengthMinutes(int lengthMinutes) {
+        this.lengthMinutes = lengthMinutes;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the countary of this movie
+    // Strips leading and trailing whitespaces
+    public void setCountary(String countary) {
+        this.countary = trimString(countary);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds actor to the set of actors for this movie
+    // Strips leading and trailing whitespaces
+    public boolean addActor(String actor) {
+        if (!actors.contains(actor)) {
+            return this.actors.add(trimString(actor));
+        } else {
+            return false;
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets whether this movie has english subtitles available or not
+    public void setEnglishSubs(boolean englishSubtitle) {
+        this.englishSubtitle = englishSubtitle;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the relative path to the image of the poster of this movie
+    // Strips leading and trailing whitespacs
+    public void setImagePath(String imagePath) {
+        this.imagePath = trimString(imagePath);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets the relative path to video file of this movie
+    // Strips leading and trailing whitespacs
+    public void setFilePath(String filePath) {
+        this.filePath = trimString(filePath);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((imdbID == null) ? 0 : imdbID.hashCode());
+        return result;
+    }
+
+    // EFFECTS: two movies are equal if and only if they have the same
+    // IMDB ID
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Movie other = (Movie) obj;
+        if (imdbID == null) {
+            if (other.imdbID != null)
+                return false;
+        } else if (!imdbID.equals(other.imdbID))
+            return false;
+        return true;
+    }
+
+    // EFFECTS: private helper method for removing
+    // leading and trailing whitespaces of a string
+    private String trimString(String str) {
+        return str.strip();
+    }
+
+    // getters
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLengthMinutes() {
+        return lengthMinutes;
+    }
+
+    public String getCountary() {
+        return countary;
+    }
+
+    public List<String> getActors() {
+        return actors;
+    }
+
+    public boolean hasEnglishSubtitle() {
+        return englishSubtitle;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+}
---
+++ CineLoca/src/tests/TestMovie.java
@@ -0,0 +1,107 @@
+package tests;
+
+import main.Movie;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMovie {
+    private Movie testMovie;
+
+    @BeforeEach
+    void setup() {
+        testMovie = new Movie("tt1392190", "Mad Max: Fury Road");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("TT1392190", testMovie.getImdbID());
+        assertEquals("Mad Max: Fury Road", testMovie.getTitle());
+        assertEquals(0, testMovie.getLengthMinutes());
+        assertEquals(null, testMovie.getDirector());
+        assertEquals(null, testMovie.getCountary());
+        assertEquals(0, testMovie.getActors().size());
+        assertFalse(testMovie.hasEnglishSubtitle());
+        assertEquals(null, testMovie.getImagePath());
+        assertEquals(null, testMovie.getFilePath());
+    }
+
+    @Test
+    void testSetID() {
+        testMovie.setID("tt0372784");
+        assertEquals("TT0372784", testMovie.getImdbID());
+    }
+
+    @Test
+    void testSetTitle() {
+        testMovie.setTitle("Batman");
+        assertEquals("Batman", testMovie.getTitle());
+    }
+
+    @Test
+    void testSetReleaseYear() {
+        testMovie.setReleaseYear(1998);
+        assertTrue(1998 == testMovie.getReleaseYear());
+    }
+
+    @Test
+    void testSetDirector() {
+        testMovie.setDirector("Christopher Nolan");
+        assertEquals("Christopher Nolan", testMovie.getDirector());
+    }
+
+    @Test
+    void testSetLengthMinutes() {
+        testMovie.setLengthMinutes(153);
+        assertTrue(153 == testMovie.getLengthMinutes());
+    }
+
+    @Test
+    void testSetCountary() {
+        testMovie.setCountary("USA");
+        assertEquals("USA", testMovie.getCountary());
+    }
+
+    @Test
+    void testAddActors() {
+        assertTrue(testMovie.addActor("Michael Caine"));
+        assertTrue(testMovie.addActor("Christopher Nolan"));
+        assertTrue(2 == testMovie.getActors().size());
+        assertEquals("Michael Caine", testMovie.getActors().get(0));
+        assertEquals("Christopher Nolan", testMovie.getActors().get(1));
+    }
+
+    @Test
+    void testAddActorsDuplicateActor() {
+        assertTrue(testMovie.addActor("Michael Caine"));
+        assertTrue(testMovie.addActor("Christopher Nolan"));
+        assertFalse(testMovie.addActor("Michael Caine"));
+        assertTrue(2 == testMovie.getActors().size());
+        assertEquals("Michael Caine", testMovie.getActors().get(0));
+        assertEquals("Christopher Nolan", testMovie.getActors().get(1));
+    }
+
+    @Test
+    void testSetEnglishSubs() {
+        testMovie.setEnglishSubs(true);
+        assertTrue(testMovie.hasEnglishSubtitle());
+        testMovie.setEnglishSubs(false);
+        assertFalse(testMovie.hasEnglishSubtitle());
+    }
+
+    @Test
+    void testSetImagePath() {
+        String path = "CineLoca/00testData/images/testMoviePoster.jpg";
+        testMovie.setImagePath(path);
+        assertEquals(path, testMovie.getImagePath());
+    }
+
+    @Test
+    void testSetFilePath() {
+        String path = "CineLoca//00testData//movie//testMovie.mp4";
+        testMovie.setFilePath(path);
+        assertEquals(path, testMovie.getFilePath());
+    }
+
+}
