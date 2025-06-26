25f47d9800049c30b2c485c03ef7fd0d47ac870d
Mahan <mahanrn@student.ubc.ca>
2025-06-26 02:35:10 -0700

    Added UI package and Flatlaf look and feel

--- CineLoca/src/main/java/Main.java
+++ CineLoca/src/main/java/Main.java
@@ -1,6 +1,18 @@
+import javax.swing.SwingUtilities;
+import com.formdev.flatlaf.FlatDarkLaf;
+
+import ui.MainWindow;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        FlatDarkLaf.setup();
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                MainWindow main = new MainWindow();
+                main.show();
+            }
+        });
     }
 }
---
+++ CineLoca/src/main/java/ui/MainWindow.java
@@ -0,0 +1,20 @@
+package ui;
+
+import javax.swing.JFrame;
+
+public class MainWindow {
+
+    private JFrame window;
+
+    public MainWindow() {
+        window = new JFrame("CineLoca");
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        window.setSize(800, 500);
+        window.setLocationRelativeTo(null);
+    }
+
+    public void show() {
+        window.setVisible(true);
+    }
+    
+}
