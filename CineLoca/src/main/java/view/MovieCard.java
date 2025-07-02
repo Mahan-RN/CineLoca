4299278be3ee26cc3d8b0a66354c3d7aa4cf1893
Mahan <mahanrn@student.ubc.ca>
2025-07-02 00:24:50 -0700

    Created MovieCard

---
+++ CineLoca/src/main/java/view/MovieCard.java
@@ -0,0 +1,14 @@
+package view;
+
+import model.Movie;
+
+// Represents an individual movie card in the MainWindow
+public class MovieCard {
+    private Movie movie;
+
+    // EFFECTS: creates a movie card to represent the given movie
+    public MovieCard(Movie movie) {
+        this.movie = movie;
+
+    }
+}
