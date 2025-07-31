ea8416c08abe0fbeef40ddaca48ff0f4a88c8691
Mahan <mahanrn@student.ubc.ca>
2025-07-30 22:59:49 -0700

    Refactored FileReader

---
+++ CineLoca/src/main/java/model/AbstractFileReader.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public abstract class AbstractFileReader {
+
+    protected String pathName;
+    protected File directory;
+    protected ArrayList<File> files;
+    protected MediaCollection collection = MediaCollection.getInstance();
+
+    // EFFECTS: creates a file reader
+    // Throws FileNotFoundException if the provided file path is invalid
+    public AbstractFileReader(String pathName) throws FileNotFoundException {
+        this.pathName = pathName;
+        initialize(pathName);
+    }
+
+    // =====================
+    // Private Helper Methods
+    // =====================
+
+    // MODIFIES: this
+    // EFFECTS: creats a directory based on the provided path. Makes a list of
+    // all files and folders in the provided directory
+    // Throws FileNotFoundException if the provided path does not exist or
+    // is a file (rather than a directory)
+    private void initialize(String pathName) throws FileNotFoundException {
+        this.directory = new File(pathName);
+        if (directory.isFile() || !directory.exists()) {
+            throw new FileNotFoundException("Provided directory is invalid: "
+                    + pathName);
+        }
+        this.files = new ArrayList<>(Arrays.asList(directory.listFiles()));
+    }
+
+    // getters
+    public String getPathName() {
+        return pathName;
+    }
+
+    public ArrayList<File> getFiles() {
+        return this.files;
+    }
+}
--- CineLoca/src/main/java/model/FileReader.java
+++ CineLoca/src/main/java/model/FileReader.java
@@ -1,24 +1,16 @@
 package model;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 // Represents a file reader to read files from the OS
-public class FileReader {
-    private String pathName;
-    private File directory;
-    private ArrayList<File> files;
-    private MediaCollection collection = MediaCollection.getInstance();
+public class FileReader extends AbstractFileReader {
 
-    // EFFECTS: creates a file reader
+    // EFFECTS: creates a file reader for Movies
     // Throws FileNotFoundException if the provided file path is invalid
     public FileReader(String pathName) throws FileNotFoundException {
-        this.pathName = pathName;
-        initialize(pathName);
+        super(pathName);
     }
 
     // MODIFIES: this, AbstractMedia, MediaCollection
@@ -46,33 +38,5 @@ public class FileReader {
                 }
             }
         }
-    }
-
-    // =====================
-    // Private Helper Methods
-    // =====================
-
-    // MODIFIES: this
-    // EFFECTS: creats a directory based on the provided path. Makes a list of
-    // all files and folders in the provided directory
-    // Throws FileNotFoundException if the provided path does not exist or
-    // is a file (rather than a directory)
-    private void initialize(String pathName) throws FileNotFoundException {
-        this.directory = new File(pathName);
-        if (directory.isFile() || !directory.exists()) {
-            throw new FileNotFoundException("Provided directory is invalid: "
-                    + pathName);
-        }
-        this.files = new ArrayList<>(Arrays.asList(directory.listFiles()));
-    }
-
-    // getters
-
-    public String getPathName() {
-        return pathName;
-    }
-
-    public ArrayList<File> getFiles() {
-        return this.files;
     }
 }
